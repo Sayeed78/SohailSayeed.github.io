9669aa15014ace9910d83968c070fb3b4c1c1179
Sohail Sayeed <sohailsayeed78@yahoo.com>
2023-07-19 19:18:30 -0400

    update

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Button
+
+function printHello() {
+    alert('Hello Akari')
+}
+
+const imageContainer = document.querySelector(".image-container");
+
+function scrollImages() {
+    imageContainer.scrollLeft += 200; // Change the scroll speed by adjusting the value (larger = faster)
+}
+
+setInterval(scrollImages, 50); // Adjust the interval (milliseconds) for smoother or faster scrolling
