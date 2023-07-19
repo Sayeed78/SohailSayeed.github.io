ad70599ccec1279f33fa3cdfc88c24970d92f98a
Sohail Sayeed <sohailsayeed78@yahoo.com>
2023-07-19 19:32:40 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// script.js
 
 function printHello() {
     alert('Hello Akari')
@@ -11,3 +11,20 @@ function scrollImages() {
 }
 
 setInterval(scrollImages, 50); // Adjust the interval (milliseconds) for smoother or faster scrolling
+
+
+// Get references to the button and container
+const toggleButton = document.getElementById('Show Images');
+const myContainer = document.getElementById('.image-container');
+
+// Add a click event listener to the button
+toggleButton.addEventListener('click', function () {
+  // Toggle the display property of the container between 'none' and 'block'
+  if (myContainer.style.display === 'none') {
+    myContainer.style.display = 'block';
+    toggleButton.textContent = 'Hide Container';
+  } else {
+    myContainer.style.display = 'none';
+    toggleButton.textContent = 'Show Container';
+  }
+});
