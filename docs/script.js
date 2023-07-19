a7de1a951a806dc7197566926bdaa39eb339526f
Sohail Sayeed <sohailsayeed78@yahoo.com>
2023-07-19 16:19:41 -0400

    update

--- docs/script.js
+++ docs/script.js
@@ -3,7 +3,7 @@
 const imageContainer = document.querySelector('.image-container');
 
 function scrollImages() {
-    imageContainer.scrollLeft += 2; // Change the scroll speed by adjusting the value (larger = faster)
+    imageContainer.scrollLeft += 200; // Change the scroll speed by adjusting the value (larger = faster)
 }
 
 setInterval(scrollImages, 50); // Adjust the interval (milliseconds) for smoother or faster scrolling
